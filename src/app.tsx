e461debda84f2ea2ca1411d03dbb952e81f940d1
DoRex <dore250393@gmail.com>
2019-03-19 21:30:53 +0100

    initial project setup

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const root: HTMLElement = document.getElementById('root')
+
+class Test extends React.Component<any,any>{
+    render(): JSX.Element {
+        return <div>hello world!</div>
+    }
+}
