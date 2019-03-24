c5dc16b307d93ea3e89e058784f7ee66f6585456
DoRex <dore250393@gmail.com>
2019-03-24 10:39:12 +0100

    setting up react, react dom, lite server,...

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const root = document.getElementById('root');
+class Test extends React.Component {
+    render() {
+        return React.createElement("div", null, "hello world!");
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 const root: HTMLElement = document.getElementById('root')
 
-class Test extends React.Component<any,any>{
-    render(): JSX.Element {
-        return <div>hello world!</div>
+interface TestProps {
+    hello: string;
+}
+
+class Test extends React.Component<TestProps, any>{
+   public render(): JSX.Element {
+        return <div>{this.props.hello}</div>
     }
 }
+
+ReactDOM.render(<Test />, root);
